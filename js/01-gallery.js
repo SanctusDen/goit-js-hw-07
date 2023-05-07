55ed9284a66550be1bc67954b6c0c0f9a811c6e1
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-05-07 14:55:08 +0300

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const createGallery = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+  </a>
+    </li>`).join("");
+list.insertAdjacentHTML("afterbegin", createGallery);
+list.addEventListener("click", onAddImg);
+
+function onAddImg(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName === 'IMG') {
+        const addImg = evt.target.dataset.source;
+        const instance = basicLightbox.create(`<img src="${addImg}" 
+         alt="${evt.target.alt}"> `,
+            {
+                onClose: function () {
+                    document.removeEventListener("keydown", closeByEscape);
+                },
+            }
+        );
+    instance.show();
+    document.addEventListener("keydown", closeByEscape);
+    function closeByEscape(evt) {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    }
+    }
+    else {
+    return;
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryAll = document.querySelector(".gallery");
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryAll.insertAdjacentHTML("afterbegin", galleryEl);
+
+new SimpleLightbox(".gallery__link", {captionDelay: 250});
